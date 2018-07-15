3aeea08754d44a55be3376e2490941b042b5fbcd
duruofei <120278473@qq.com>
2018-07-15 20:54:15 +0800

    change basic

--- lib.js
+++ lib.js
@@ -1,3 +1,11 @@
+function addElementClickById(id,someThing)
+{
+	var element = document.getElementById(id);
+	if(element!=null)
+	{
+		element.addEventListener("click",someThing);
+	}
+}
 function getScriptSource(id)
 {
 	return document.getElementById(id).textContent;
@@ -18,5 +26,20 @@ function addShader(gl,vShaderSource,fShaderSource)
 	gl.attachShader(shaderProgram,vShader);
 	gl.attachShader(shaderProgram,fShader);
 	gl.linkProgram(shaderProgram);
-	gl.useProgram(shaderProgram);
+	return shaderProgram;
 }
+function clearBackground(gl,r=0,g=0,b=0)
+{
+	gl.clearColor(r,g,b,1);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+function render(clean,gl,shaderProgram,draw,r=0,g=0,b=0)
+{
+	if(clean)
+	{
+		clearBackground(gl,r,g,b);
+	}
+	gl.useProgram(shaderProgram);
+	draw(gl);
+}
+
